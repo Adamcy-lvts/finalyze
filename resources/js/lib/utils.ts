99f5af8dc67ea28871f3065a62b51dd6ec179192
Adamcy-lvts <lv4mj1@gmail.com>
2025-11-29 09:47:47 +0100

    fix: add valueUpdater helper for tanstack table

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,15 @@
 import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import type { Updater } from '@tanstack/vue-table';
+import type { Ref } from 'vue';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
+}
+
+// Helper to update tanstack state refs
+export function valueUpdater<T>(updaterOrValue: Updater<T>, ref: Ref<T>) {
+    ref.value = typeof updaterOrValue === 'function'
+        ? (updaterOrValue as (old: T) => T)(ref.value)
+        : updaterOrValue;
 }
