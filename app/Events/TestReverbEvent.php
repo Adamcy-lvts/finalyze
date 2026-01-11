797932307d4e8e4b812391337e399b3ece736625
Adamcy-lvts <lv4mj1@gmail.com>
2026-01-11 15:31:30 +0100

    add reverb test class

---
+++ app/Events/TestReverbEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TestReverbEvent implements ShouldBroadcast
+{
+    public function broadcastOn()
+    {
+        return new Channel('test-channel');
+    }
+
+    public function broadcastAs()
+    {
+        return 'TestReverbFired';
+    }
+
+    public function broadcastWith()
+    {
+        return ['message' => 'Reverb is working!'];
+    }
+}
